de732e60e7f210ba1cdd982a4381f4c422f89080
i-kaseta-tou-mpampa <82847417+i-kaseta-tou-mpampa@users.noreply.github.com>
2021-04-20 02:55:58 +0300

    Update jquery.cassette.js

--- js/jquery.cassette.js
+++ js/jquery.cassette.js
@@ -1,15 +1,4 @@
-/**
- * jquery.cxassette.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2012, Codrops
- * http://www.codrops.com
- */
-
-( function( window, $, undefined ) {
+window.addEventListener('load', (event) =>{
 	
 	var aux = {
 	
@@ -997,4 +986,4 @@
 		
 	};
 	
-} )( window, jQuery );
+} );
