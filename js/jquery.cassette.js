15cdd74cf41aefd16f40d021ada0f90001d4867c
i-kaseta-tou-mpampa <82847417+i-kaseta-tou-mpampa@users.noreply.github.com>
2021-04-20 01:40:15 +0300

    jquery removed

--- js/jquery.cassette.js
+++ js/jquery.cassette.js
@@ -1,15 +1,5 @@
-/**
- * jquery.cxassette.js v1.0.0
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2012, Codrops
- * http://www.codrops.com
- */
 
-( function( window, $, undefined ) {
+window.addEventListener('load', (event) =>  {
 	
 	var aux = {
 	
@@ -997,4 +987,4 @@
 		
 	};
 	
-} )( window, jQuery );
+} );
